c62b77941a61296eb67980298fe472d5a4fe1975
AndriySt92 <andriyua-1992@mail.ru>
2024-06-07 12:11:09 +0300

    Init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="App">
+      <h1 className="text-3xl bg-slate-300 font-bold underline">Hello world!</h1>
+    </div>
+  )
+}
+
+export default App
